f33e98f62ba250f3fe2a7c2dbc38107d43d57c9e
Wiktor Wilinski <wilinskiwiktor2005@gmail.com>
2023-06-25 13:32:31 +0200

    Usunięcie parametru power z enumu Rank

--- src/Rank.java
+++ src/Rank.java
@@ -1,16 +1,11 @@
 public enum Rank {
     AS(Ability.CHANGE_SUIT), TWO(Ability.PLUS_2), THREE(Ability.PLUS_3), FOUR(Ability.WAIT),
-    FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10),
+    FIVE(null), SIX(null), SEVEN(null), EIGHT(null), NINE(null), TEN(null),
     Q(Ability.ON_EVERYTHING), J(Ability.DEMAND), K(Ability.KING_EXCEPTION);
-
-    public int power;
-    public Ability ability;
+    public final Ability ability;
 
     Rank(Ability ability) {
         this.ability = ability;
     }
 
-    Rank(int power) {
-        this.power = power;
-    }
 }
